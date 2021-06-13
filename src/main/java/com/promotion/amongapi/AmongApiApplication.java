0fa5963ec832679a771b7c1c8deedcfb09e8ec9c
Xylope <XylopeOfficial@gmail.com>
2021-06-13 15:09:54 +0900

    Initial commit - add spring boot and 3rd parties

---
+++ src/main/java/com/promotion/amongapi/AmongApiApplication.java
@@ -0,0 +1,13 @@
+package com.promotion.amongapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmongApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmongApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promotion/amongapi/AmongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion.amongapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

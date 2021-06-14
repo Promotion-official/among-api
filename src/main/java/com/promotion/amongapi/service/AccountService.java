b6c9c6866645144ceb2d9081d0019c7aba9092e7
Xylope <XylopeOfficial@gmail.com>
2021-06-14 13:27:30 +0900

    Partial implementation of AccountService - Reliably revise Account Service Test for some functions during planning - Implement AccountService that passes AccountServiceTest

--- src/main/java/com/promotion/amongapi/service/AccountService.java
+++ src/main/java/com/promotion/amongapi/service/AccountService.java
@@ -2,10 +2,14 @@ package com.promotion.amongapi.service;
 
 import com.promotion.amongapi.dto.AccountDto;
 import com.promotion.amongapi.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.Max;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -25,5 +29,34 @@ public class AccountService {
 
     public AccountDto get(String email) {
         return repository.getAccountDtoByEmail(email);
+    }
+
+    public void updateStudentId(String email, int studentId) {
+        int[] studentIdData= translateStudentIdToAccountDto(studentId);
+        AccountDto dto = get(email);
+
+        dto.setGeneration(studentIdData[TranslateArray.GEN.index]);
+        dto.setClazz(studentIdData[TranslateArray.CLAZZ.index]);
+        dto.setNumber(studentIdData[TranslateArray.NUMBER.index]);
+
+        repository.save(dto);
+    }
+
+    private int[] translateStudentIdToAccountDto(@Max(9999) int studentId) {
+        Calendar cal = Calendar.getInstance();
+        int currentYear = cal.get(Calendar.YEAR);
+        int grade = studentId / 1000;
+        int gen = currentYear - 2015 - grade;
+        int clazz = (studentId % 1000) / 100;
+        int number = studentId % 100;
+
+        return new int[]{gen, grade, clazz, number};
+    }
+
+    @AllArgsConstructor
+    private enum TranslateArray {
+        GEN(0), GRADE(1), CLAZZ(2), NUMBER(3);
+
+        private final int index;
     }
 }
--- src/test/java/com/promotion/amongapi/service/AccountServiceTest.java
+++ src/test/java/com/promotion/amongapi/service/AccountServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -36,10 +37,11 @@ public class AccountServiceTest {
         loremIpsum = LoremIpsum.getInstance();
         testDtos = new ArrayList<>();
 
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR); //to calculate max generation
         testDtos = Stream.generate(() -> AccountDto.builder()
                 .name(loremIpsum.getName())
                 .email(loremIpsum.getEmail())
-                .generation(random.nextInt(100))
+                .generation(random.nextInt(currentYear - 2016))
                 .clazz(random.nextInt(4))
                 .number(random.nextInt(20))
                 .build()).limit(10).collect(Collectors.toList());
@@ -71,5 +73,57 @@ public class AccountServiceTest {
         assertEquals(service.get(email), getDto);
         //Check AccountService delegated the getAccount logic to AccountRepository
         verify(repository).getAccountDtoByEmail(email);
+    }
+
+    @Test
+    public void testCountName() {
+        //TODO 테스트 코드 작성 | 2021-06-14
+    }
+
+    @Test
+    public void testCountGeneration() {
+        //TODO 테스트 코드 작성 | 2021-06-14
+    }
+
+    @Test
+    public void testCountGrade() {
+        //TODO 테스트 코드 작성 | 2021-06-14
+    }
+
+    @Test
+    public void testCountClazz() {
+        //TODO 테스트 코드 작성 | 2021-06-14
+    }
+
+    @Test
+    public void testUpdateStudentId() {
+        //Setting test environment
+        int studentId = 2218;
+        int idxOfGetDto = new Random().nextInt(10);
+        AccountDto testDto = testDtos.get(idxOfGetDto);
+        when(repository.getAccountDtoByEmail(testDto.getEmail())).thenReturn(testDto);
+
+        //Predict the results to create a comparison group
+        //---->translateStudentIdToAccountDto 의 로직을 수동으로 구현한다
+        Calendar cal = Calendar.getInstance();
+        int currentYear = cal.get(Calendar.YEAR);
+        int grade = studentId / 1000;
+        int gen = currentYear - 2015 - grade;
+        int clazz = (studentId % 1000) / 100;
+        int number = studentId % 100;
+        //수동구현한 학번 예상값을 통하여 반환 예상값을 구한다
+        AccountDto expectedResultDto = AccountDto.builder()
+                .name(testDto.getName()).email(testDto.getEmail())
+                .generation(gen).clazz(clazz).number(number)
+                .build();
+        String email = testDto.getEmail();
+
+        //Check method is run correctly
+        service.updateStudentId(email, studentId);
+        verify(repository).save(expectedResultDto);
+
+        log.info("AccountServiceTest - testUpdateStudentId");
+        log.info("insert value : " + testDto);
+        log.info("expected & real result : " + expectedResultDto);
     }
 }

9478a88dd33a368f8e89f69a658d550953ba7bd6
Xylope <XylopeOfficial@gmail.com>
2021-07-05 14:30:02 +0900

    code review 에 따라 code refactoring 진행

--- src/main/java/com/promotion/amongapi/advice/exception/AuthorizeKeyNotFoundException.java
+++ src/main/java/com/promotion/amongapi/advice/exception/AuthorizeKeyNotFoundException.java
@@ -1,4 +1,10 @@
 package com.promotion.amongapi.advice.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public class AuthorizeKeyNotFoundException extends RuntimeException {
+    @Getter
+    String authorizeKey;
 }

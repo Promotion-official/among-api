4963feabad05b041ee7bfbd199fa129c2e723286
JeongYongWoo <41749052+Sinawe4@users.noreply.github.com>
2021-08-25 23:33:28 +0900

    JwtProperties의 ignore가 먹히지 않았음

--- src/main/java/com/promotion/amongapi/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.promotion.amongapi.jwt;
-
-import lombok.Data;
-
-@Data
-public class JwtProperties {
-    private String secretKey = "72172065B70742D3AF7D8D030466F785E314D40802C7ACED950D654255CAB1AC";
-}
